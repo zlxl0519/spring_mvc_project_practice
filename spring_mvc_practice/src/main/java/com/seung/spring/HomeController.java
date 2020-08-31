d86e7b96fcc1e509c7864e9f5377644f32b91fc6
seunghui <zlxl0519@naver.com>
2020-08-31 11:40:28 +0900

    Spring_mvc_practice 프로젝트 기본 구성함

---
+++ spring_mvc_practice/src/main/java/com/seung/spring/HomeController.java
@@ -0,0 +1,15 @@
+package com.seung.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home.do")
+	public String home() {
+		
+		return "home";
+	}
+	
+}
---
+++ spring_mvc_practice/src/main/webapp/resources/js/my-util.js
@@ -0,0 +1,5 @@
+/* js/ my-util.js*/
+//유틸리티 - 편리하게 쓸수있게 미리 만들어 논것
+function select(sel){
+    return document.querySelector(sel);
+}
